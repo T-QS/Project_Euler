df713f1d300184aa62e8c1fb814437c314310701
T-QS <719756541@qq.com>
2018-07-09 14:21:33 +0800

    add 6

---
+++ code/6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int sum1 = 0, sum2 = 0;
+    for (int i = 1; i <= 100; ++i) {
+        sum1 += i;
+        sum2 += i * i;
+    }
+    printf("%d\n", sum1 * sum1 - sum2);
+    return 0;
+}
